178bbcb5c92eb04c0cf5190e9d3fce037912953e
Charles HUSQUIN <charles@MacBook-Pro-de-Charles.local>
2017-06-05 20:13:07 +0200

    add cors header / GET / POST

--- index.js
+++ index.js
@@ -2,9 +2,28 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', function(req, res){
-    res.end('Hello world');
+let todos = [];
+
+/*
+    Set the CORS headers
+ */
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+app.get('/todos', function(req, res){
+    console.log('got a GET request');
+    res.end(JSON.stringify(todos));
+});
+
+app.post('/todos', function(req, res){
+    console.log('got a POST request');
+    todos.push(JSON.parse(req.body));
+    res.end(req.body);
 });
 
 app.listen(3000);
-consol.log('server listening on port 3000');
+console.log('server listening on port 3000');
